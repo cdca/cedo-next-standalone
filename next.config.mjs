82429e745833e0788f2b0bd28cc7585f10969a52
Cedomir Sobic <cedomir.sobic@gmail.com>
2024-02-15 12:45:43 +0100

    Add docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "standalone",
+  };
+  
+  export default nextConfig;
+  
